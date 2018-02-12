0f01ef93deef34c497cdc88681081ff1d135af18
SigmazGFX <sigmaz_1@yahoo.com>
2018-02-12 13:39:53 -0500

    No more Touch TFT
    Removed the Touch TFT going to steer towards simpler I2C LCD and buttons. 
    Less pins wasted.

--- PhoneTester.ino
+++ PhoneTester.ino
@@ -1,24 +1,9 @@
 /*/////////////////////////////////////////////////////////////////////////
-// Arduino UNO phone device tester with TFT Touch shield interface       //   
-// For Arduino, QCX601 SLIC board and SIM900 or SIM800 breakout board    //
+// Arduino UNO phone device tester with display & QCX601 SLIC board      //
 // 2018 -SigmazGFX                                                       // 
 ///////////////////////////////////////////////////////////////////////////
 */
-//---------------------------
-// Start by including the TFT touch screen support
-#include <stdint.h>
-#include <SeeedTouchScreen.h>
-#include <TFTv2.h>
-int ColorPaletteHigh = 30;
-int color = WHITE;  //Paint brush color
-unsigned int colors[8] = {BLACK, RED, GREEN, BLUE, CYAN, YELLOW, WHITE, GRAY1};
 
-// For better pressure precision, we need to know the resistance
-// between X+ and X- Use any multimeter to read it
-// The 2.8" TFT Touch shield has 300 ohms across the X plate
-
-TouchScreen ts = TouchScreen(XP, YP, XM, YM); //init TouchScreen port pins
-//------------------------------
 
 //DTMF library to handle DTMF via audio sampling
 #include <DTMF.h>
@@ -49,24 +34,21 @@ DTMF dtmf = DTMF(n,sampling_rate);
 #define ACTIVE_CALL 3
 #define GETTING_NUMBER 4
 
-int sensorPin = A5;     // Analog pin used to sample audio for DTMF decoding
+int sensorPin = A0;     // Analog pin used to sample audio for DTMF decoding
 #define rflPin 2        // RFL pin to QCX601 pin J1-1, reverse signal (LOW)
 #define hzPin 3         // 25Hz pin to QCX601 pin J1-2 (25Hz signal for ringing) _-_-_-_-_
 #define rcPin 8         // RC pin to QCX601 pin J1-3, ring control, HIGH when ringing --___--___--___
 #define shkPin 9        // switch hook pin from QCX601 pin J1-4
+#define ringTestPin 12  // Pin assigned to start ring test
 
-                        // Change the pins above as is suits your project
-                        // For connection to GSM board, pin 2 goes to TX and pin 3 to RX
-                        // To change this, edit GSM.cpp in the GSM library
-                        
-#define oscInterval 20
+#define oscInterval 20 
 #define ringInterval 6000
 #define ringDuration 1200
 #define statusCheckInterval 1000
 
 #define tNewDig 500     // time since last SHK rising edge before counting for next digit
 #define tHup 2000       // time since last SHK falling edge before hanging up/flushing number
-#define tComplete 6000  // time since last SHK rising edge before starting call
+#define tComplete 6000  // time since last SHK rising edge before quit collecting #'s
 #define tDebounce 15    // debounce time
 
 unsigned long currentMillis = 0L;
@@ -76,45 +58,39 @@ unsigned long statusPreviousMillis = 0L;
 unsigned long lastShkDebounce = 0L;
 unsigned long lastShkRise = 0L;
 unsigned long lastShkFall = 0L;
-
+int hookPrompt = 0;
 int shkState = 0;           // LOW is on hook, HIGH is off hook
 int edge;
 int lastShkReading = 0;
 int currentShkReading = 0;
 
+int ringTest; 
 byte pulses = 0;
 byte digit = 0;
 String number = "";
 byte digits = 0;            // the number of digits we have collected
 char numArray[11];
-byte gsmStatus;
+//byte gsmStatus;
 byte state;
 
-//---------------------------
+
+////////////////////////////////////////////////////////////////////////////
+// 
+//  SETUP
+//
 void setup() {
   
-  Tft.TFTinit();  //init TFT library
-  
   pinMode(sensorPin,INPUT);
-  pinMode(shkPin, INPUT);
+  pinMode(shkPin, INPUT_PULLUP);
   pinMode(hzPin, OUTPUT);
   pinMode(rcPin, OUTPUT);
   pinMode(rflPin, OUTPUT);
   digitalWrite(rflPin, 0);
-  #if defined(SLIC_TEST)
   pinMode(ringTestPin, INPUT_PULLUP);
-  #endif
 
-  Serial.begin(9600); // start serial for debug monitor
-  Serial.println("\nHello, I am your telephone.");
-  #if defined(GSM_MODULE)
-  if (gsm.begin(2400)) {
-    Serial.println("\nstatus=READY");
-  } else {
-    Serial.println("\nstatus=IDLE");
-  }
-  #endif
-  state = IDLE_WAIT;
+  Serial.begin(115200); // start serial for debug monitor
+  Serial.println("\nTelephone Tester Ready..");
+   state = IDLE_WAIT;
 }
 int nochar_count = 0;
 float d_mags[8];
@@ -147,10 +123,8 @@ void loop() {
   lastShkReading = currentShkReading;
 
   if ((unsigned long)(currentMillis - statusPreviousMillis) >= statusCheckInterval) {
-   
-    #if defined(SLIC_TEST)
+    // Time to check control status 
     ringTest = !digitalRead(ringTestPin);
-    #endif
     statusPreviousMillis = currentMillis;
   }
 
@@ -160,27 +134,16 @@ void loop() {
   //  STATE IDLE_WAIT
   //
   if (state == IDLE_WAIT) {
-    // wait for incoming call or picking up reciever
-    #if defined(GSM_MODULE)
-    if (gsmStatus == CALL_INCOM_VOICE) {
-      Serial.println("Incoming call. Ringing.");
-      state = RINGING;
-    }
-    if (gsmStatus == CALL_ACTIVE_VOICE) {
-      // this should not happen
-      // hanging up must have failed, try again
-      call.HangUp();
-      delay(2000); 
-    }
-    #endif
-    #if defined(SLIC_TEST)
+    // wait for ring test or picking up reciever
     if (ringTest) {
-      Serial.println("Incoming call. Ringing.");
+      Serial.println("Ringing.");
       state = RINGING;
     }
-    #endif    
+   // #endif    
     if (shkState == HIGH) {
-      Serial.println("Off hook. Dial your number.");
+           if (hookPrompt == 0){
+      Serial.println("Off hook. Dial test ready. ");
+      }
       state = GETTING_NUMBER;
     }
   }
@@ -189,7 +152,6 @@ void loop() {
   //  STATE RINGING
   //
   if (state == RINGING) {  
-    // ring until reciever is off hook or call disconnected
     // Ringing interval 
     // How much time has passed, accounting for rollover with subtraction!
     if ((unsigned long)(currentMillis - ringPreviousMillis) >= ringInterval) {
@@ -213,69 +175,36 @@ void loop() {
     if (shkState == HIGH) {
       digitalWrite(rcPin, 0); // stop ringing
       digitalWrite(hzPin, 0);
-      // tell GSM board to pick up
-      Serial.println("Picking up. Call initiated.");
-//      #if defined(GSM_MODULE)
-//      call.PickUp();
-//      #endif   
-      state = ACTIVE_CALL; 
+      Serial.println("Answered. Loop initiated.");
+     state = ACTIVE_CALL; 
     }
-//    #if defined(GSM_MODULE)
-//    if (gsmStatus != CALL_INCOM_VOICE) {
-//      digitalWrite(rcPin, 0); // stop ringing
-//      digitalWrite(hzPin, 0);
-//      Serial.println("Caller gave up. Going back to idle.");
-//      state = IDLE_WAIT;
-//    }
-//    #endif
-   
-///////////////////////////////////////////////
-//  RING TEST ----------------------------     
-    #if defined(SLIC_TEST)
     if (!ringTest) {
       digitalWrite(rcPin, 0); // stop ringing
       digitalWrite(hzPin, 0);
-      Serial.println("Caller gave up. Going back to idle.");
+      Serial.println("Going back to idle.");
+      Serial.println("\nTelephone Tester Ready..");
       state = IDLE_WAIT;
     }
-    #endif
-  }
-
-
   
+  }
   ////////////////////////////////////////////////////////////////////////////
   // 
   //  STATE ACTIVE_CALL
   //
   if (state == ACTIVE_CALL) {
-    // keep connection until on-hook or call disconnected
+    // keep state until on-hook 
     if ((shkState == LOW) && ((unsigned long)(currentMillis - lastShkFall) >= tHup)) {
-      // tell GSM board to disconnect call, flush everything, then go idle
+      //flush everything, then go idle
       Serial.println("Hanging up. Going idle.");
-//      #if defined(GSM_MODULE)
-//      call.HangUp();
-//      #endif
       flushNumber();
-      delay(1000); // wait a sec before going to next state to make sure GSM module keeps up
+      Serial.println("\nTelephone Tester Ready..");
       state = IDLE_WAIT;
     }
-    #if defined(GSM_MODULE)
-    if (gsmStatus == CALL_NONE) {
-      Serial.println("Call disconnected. Going idle.");
+   if (!ringTest) {
       flushNumber();
       state = IDLE_WAIT;
     }
-    #endif
-    #if defined(SLIC_TEST)
-    if (!ringTest) {
-      Serial.println("Call disconnected. Going idle.");
-      flushNumber();
-      state = IDLE_WAIT;
-    }
-    #endif
   }
-
-  
   ////////////////////////////////////////////////////////////////////////////
   // 
   //  STATE GETTING_NUMBER
@@ -285,11 +214,17 @@ void loop() {
     // if single digit, fetch stored number
     // then make call
 
+    if (pulses < 1)
+    {
+      detectTones();
+    }
+      
+
     if (pulses && (unsigned long)(currentMillis - lastShkRise) > tNewDig) {
       // if there are pulses, check rising edge timer for complete digit timeout
-      digit = pulses - 1; // one pulse is zero, ten pulses is nine (swedish system)
+      //digit = pulses - 1; // one pulse is zero, ten pulses is nine (swedish system)
       // for systems where ten pulses is zero, use code below instead:
-      // digit = pulses % 10;
+      digit = pulses % 10;
       Serial.println(digit); // just for debug
       // add digit to number string
       number += (int)digit;
@@ -305,37 +240,38 @@ void loop() {
       edge = 0;
     }
     
-    if ((digits && (shkState == HIGH) && ((unsigned long)(currentMillis - lastShkRise) > tComplete)) || digits == 10) {
-      // if completed number (full 10 digits or timeout with at least one digit)
-      // check if shortnumber/fave and then tell GSM board to initiate call 
-      //if (digits == 1) getFave();
-      Serial.print("Number complete, calling: ");
+    if ((digits && (shkState == HIGH) && ((unsigned long)(currentMillis - lastShkRise) /* > tComplete)) || digits == 10 */))) {
+      Serial.print("Number dialed: ");
       Serial.println(number);
-      number.toCharArray(numArray, 11);
-      #if defined(GSM_MODULE)
-      call.Call(numArray);
-      #endif
-      delay(1000); // wait a sec before going to next state to make sure GSM module keeps up
-      state = ACTIVE_CALL;
+      //number.toCharArray(numArray, 11);
+     hookPrompt = 1;    
+     state = ACTIVE_CALL;
     }
     if ((shkState == LOW) && (unsigned long)(currentMillis - lastShkFall) > tHup) {
       // reciever on hook, flush everything and go to idle state
       flushNumber();
-      Serial.println("On hook. Flushing everything. Going idle.");
+      Serial.println("On hook.");
+      Serial.println("\nTelephone Tester Ready..");
+      hookPrompt = 0;
       state = IDLE_WAIT;
     }
   }
 }  // END OF MAIN LOOP
 
-void DTMFdetect()
+
+////////////////////////////////////////////////////////////////////////////
+// 
+//  FUNCTIONS
+//
+
+void detectTones()
 {
   char thischar;
-  
-  // This reads N samples from sensorpin (must be an analog input)
+    // This reads N samples from sensorpin (must be an analog input)
   // and stores them in an array within the library. Use while(1)
   // to determine the actual sampling frequency as described in the
   // comment at the top of this file
-  /*  while(1) */dtmf.sample(sensorPin);
+  /* while(1) */dtmf.sample(sensorPin);
   
   // The first argument is the address of a user-supplied array
   // of 8 floats in which the function will return the magnitudes
@@ -378,10 +314,7 @@ void DTMFdetect()
     if(nochar_count > 30000)nochar_count = 51;
   }
 }
-////////////////////////////////////////////////////////////////////////////
-// 
-//  FUNCTIONS
-//
+
 
 void flushNumber() {
   digits = 0;
@@ -389,5 +322,3 @@ void flushNumber() {
   pulses = 0;
   edge = 0;
 }
-
-
